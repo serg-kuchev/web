b4dc53010fdfb745d86e8f90e223c2e416d2f23c
Сергей Кучев <kuchevwork@yandex.ru>
2021-06-25 15:14:55 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+import config
+import resources
+
+app = Flask(__name__)
+app.config.from_mapping(config.CONFIG)
+db = SQLAlchemy(app)
+
+api = Api(app)
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
